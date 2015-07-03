fa4ccbece8c54280a0ba0bcaf47c476e27b0fa12
RozebMomin <rozebm@gmail.com>
2015-07-03 19:46:35 -0400

    Changed Versioning

--- plugin.video.SaudiQuranTV.en/default.py
+++ plugin.video.SaudiQuranTV.en/default.py
@@ -2,4 +2,4 @@ import xbmcaddon, util
 
 addon = xbmcaddon.Addon('plugin.video.SaudiQuranTV.en')
 
-util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), 'rtsp://livestreaming2.itworkscdn.net/squranlive/squran_360p')
+xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/?action=play_video&videoid=Yu2lmo3TrfY)')
