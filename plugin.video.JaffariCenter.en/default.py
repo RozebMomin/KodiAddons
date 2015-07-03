c2a5fa63ca33fbf9c4500a31b0a673434fe998e8
RozebMomin <rozebm@gmail.com>
2015-07-03 18:41:09 -0400

    Added NotLive Dialog Box

--- plugin.video.JaffariCenter.en/default.py
+++ plugin.video.JaffariCenter.en/default.py
@@ -8,19 +8,17 @@ import os
 addon = xbmcaddon.Addon('plugin.video.JaffariCenter.en')
 addonname = addon.getAddonInfo('name')
 
-errorLine1 = "Jaffari Center of Atlanta is not currently LIVE!"
-errorLine2 = "Please try again later."
+errorLine = "Jaffari Center of Atlanta is not currently LIVE! Please try again later! Sorry for the inconvenience."
 
 proc = subprocess.Popen(["curl", "http://xjaffari2x.api.channel.livestream.com/2.0/livestatus.json"], stdout=subprocess.PIPE)
 (out, err) = proc.communicate()
 parsed_json_data = json.loads(out)
 LiveStatus = parsed_json_data["channel"]["isLive"]
 
-
-if LiveStatus = 'false' :
-	xbmcgui.Dialog().ok(addonname, errorLine1, errorLine2)
+if LiveStatus == False :
+        xbmcgui.Dialog().ok(addonname, errorLine)
 else:
-	proc = subprocess.Popen(["curl", "http://xjaffari2x.api.channel.livestream.com/2.0/info.json"], stdout=subprocess.PIPE)
-	(out, err) = proc.communicate()
-	parsed_json = json.loads(out)
-	util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), parsed_json["channel"]["iphoneUrl"])
+        proc = subprocess.Popen(["curl", "http://xjaffari2x.api.channel.livestream.com/2.0/info.json"], stdout=subprocess.PIPE)
+        (out, err) = proc.communicate()
+        parsed_json = json.loads(out)
+        util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), parsed_json["channel"]["iphoneUrl"])
