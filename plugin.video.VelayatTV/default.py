4de45db3fed2a4cb643ebef698e0260bc88c83c6
RozebMomin <rozebm@gmail.com>
2015-08-11 20:19:57 -0400

    Other

---
+++ plugin.video.VelayatTV/default.py
@@ -0,0 +1,16 @@
+import xbmcaddon, util
+import subprocess
+from subprocess import call
+import json
+import os
+
+#proc = subprocess.Popen(["curl", "http://livestream.com/api/accounts/11436227/events/3998452/viewing_info"], stdout=subprocess.PIPE)
+#(out, err) = proc.communicate()
+
+#parsed_json = json.loads(out)
+
+addon = xbmcaddon.Addon('plugin.video.VelayatTV')
+
+URLStream = "http://96.92.96.41:1935/live/mp4:myStream/playlist.m3u8"
+
+util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
