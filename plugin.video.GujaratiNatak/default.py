0533723932541154a81e89bee638656713bb3f28
RozebMomin <rozebm@gmail.com>
2015-08-27 21:43:51 -0400

    Secondary Commit

---
+++ plugin.video.GujaratiNatak/default.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# http://www.youtube.com/user/GujaratiNatak
+#------------------------------------------------------------
+# License: GPL (http://www.gnu.org/licenses/gpl-3.0.html)
+# Based on code from youtube addon
+#------------------------------------------------------------
+
+import os
+import sys
+import plugintools
+import xbmc,xbmcaddon,util
+import xbmcgui
+from addon.common.addon import Addon
+import subprocess
+from subprocess import call
+import json
+
+addonID = 'plugin.video.GujaratiNatak'
+addon = Addon(addonID, sys.argv)
+local = xbmcaddon.Addon(id=addonID)
+icon = local.getAddonInfo('icon')
+
+YOUTUBE_CHANNEL_ID = "UC7AqvicrJ22LPClccTPFoHw"
+
+# Entry point
+def run():
+    plugintools.log("GujaratiNatak.run")
+    
+    # Get params
+    params = plugintools.get_params()
+    
+    if params.get("action") is None:
+        main_list(params)
+    else:
+        action = params.get("action")
+        exec action+"(params)"
+    
+    plugintools.close_item_list()
+
+# Main menu
+def main_list(params):
+    plugintools.log("GujaratiNatak.main_list "+repr(params))
+
+    plugintools.add_item( 
+        #action="", 
+        title="Gujarati Natak [ Full ]",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID+"/",
+        thumbnail=icon,
+        folder=True )
+
+run()
