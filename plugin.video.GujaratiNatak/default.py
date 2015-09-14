0b52073839edb62d0708dd600e1591ba7a3a3ec3
RozebMomin <rozebm@gmail.com>
2015-09-13 21:11:04 -0400

    updated repo

--- plugin.video.GujaratiNatak/default.py
+++ plugin.video.GujaratiNatak/default.py
@@ -1,58 +1,73 @@
-import urllib,urllib2,re,xbmcplugin,xbmcgui
-import os
-import requests
-from bs4 import BeautifulSoup
-import re
+# import urllib,urllib2,re,xbmcplugin,xbmcgui
+# import os
+# import requests
+# from bs4 import BeautifulSoup
+# import re
 
-#YouTube Gujarati Natak Playlist
-url = "https://www.youtube.com/playlist?list=PL7ueRli5dEU8jUZzyucuYQOllebth1x07"
-r = requests.get(url)
-data = r.text
+# #YouTube Gujarati Natak Playlist
+# url = "https://www.youtube.com/playlist?list=PL7ueRli5dEU8jUZzyucuYQOllebth1x07"
+# r = requests.get(url)
+# data = r.text
 
-soup = BeautifulSoup(data)
+# soup = BeautifulSoup(data)
 
-#Define Empty Variables
-dataTitles = []
-dataLinks = []
-dataThumbnails = []
-i=0
+# #Define Empty Variables
+# dataTitles = []
+# dataLinks = []
+# dataThumbnails = []
+# i=0
 
-#Define BeautifulSoup Queries
-videoTitle = soup.find_all("a", {"class","pl-video-title-link"})
-videoLink = soup.find_all("a", {"class","pl-video-title-link"}, href=True)
-videoThumbnail = soup.find_all("span", {"class","yt-thumb-clip"})
+# #Define BeautifulSoup Queries
+# videoTitle = soup.find_all("a", {"class","pl-video-title-link"})
+# videoLink = soup.find_all("a", {"class","pl-video-title-link"}, href=True)
+# videoThumbnail = soup.find_all("span", {"class","yt-thumb-clip"})
+#------------------------------------------------------------
+# import sys
+# import urlparse
+# import urllib,urllib2,re,xbmcplugin,xbmcgui
+# import os
+# import requests
+# from bs4 import BeautifulSoup
+# import re
 
-def CATEGORIES():
-      addDir("ONE","",1,"")
-      addDir( "TWO","",1,"")
-      addDir( "THREE","",1,"")
-      addDir("FOUR","",1,"")
+# #YouTube Gujarati Natak Playlist
+# url = "https://www.youtube.com/playlist?list=PL7ueRli5dEU8jUZzyucuYQOllebth1x07"
+# r = requests.get(url)
+# data = r.text
 
-def addDir(name,url,mode,iconimage):
-    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"name="+urllib.quote_plus(name)
-    liz=xbmcgui.ListItem(unicode(name), iconImage="DefaultFolder.png",thumbnailImage=iconimage)
-    liz.setInfo( type="Video", infoLabels={ "Title": name })
-    ok=xbmcplugin.addDirectory(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
-    return ok
+# soup = BeautifulSoup(data)
 
-def INDEX(url):
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
-        match=re.compile('').findall(link)
-        for thumbnail,url,name in match:
-                addDir(name,url,2,'')
+# #Define Empty Variables
+# dataTitles = []
+# dataLinks = []
+# dataThumbnails = []
+# i=0
 
-if mode==None or url==None or len(url)<1:
-        print ""
-        CATEGORIES()
- 
-elif mode==1:
-        print ""+url
-        INDEX(url)
- 
-elif mode==2:
-        print ""+url
-        VIDEOLINKS(url,name)
+# #Define BeautifulSoup Queries
+# # videoTitle = soup.findAll("a",{"class","pl-video-title-link"})
+# # videoLink = soup.find_all("a",{"class","pl-video-title-link"},href=True)
+# # videoThumbnail = soup.find_all("span",{"class","yt-thumb-clip"})
+
+
+#########################################
+import xbmcaddon, util
+import sys
+import xbmcgui
+import xbmcplugin
+
+addon = xbmcaddon.Addon('plugin.video.GujaratiNatak')
+
+addon_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+#url = 'https://www.youtube.com/watch?v=-fppIc3tjUg'
+url = "plugin://plugin.video.youtube/play/?video_id=-fppIc3tjUg"
+li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
+
+url = "plugin://plugin.video.youtube/play/?video_id=kqANd2aw8Mc"
+li = xbmcgui.ListItem('My Second Video!', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
