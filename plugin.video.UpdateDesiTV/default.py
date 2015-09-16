0584386dcaff8c08e1bdc1e0c3d57314f5b16673
RozebMomin <rozebm@gmail.com>
2015-09-15 21:36:20 -0400

    updated repo

--- plugin.video.UpdateDesiTV/default.py
+++ plugin.video.UpdateDesiTV/default.py
@@ -6,7 +6,8 @@ __addonname__ = __addon__.getAddonInfo('name')
 __icon__ = __addon__.getAddonInfo('icon')
  
 line1 = "Checking for updates..."
-time = 5000 #in miliseconds
+time = 50000 #in miliseconds
  
 xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, time, __icon__))
 xbmc.executebuiltin("UpdateAddonRepos", True)
+ActivateWindow(Home)
