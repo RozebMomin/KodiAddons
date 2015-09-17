8396c0870c19c83d861f2a8f8e3e448d97890c91
RozebMomin <rozebm@gmail.com>
2015-09-17 18:19:08 -0400

    updated repo

--- plugin.video.AajTak/default.py
+++ plugin.video.AajTak/default.py
@@ -9,22 +9,23 @@ import re
 
 addon = xbmcaddon.Addon('plugin.video.AajTak')
 
-r = requests.get("http://hellotv.in/livetv/play?classid=1887&parentId=0&name=MOST%20VISITED")
+# r = requests.get("http://hellotv.in/livetv/play?classid=1887&parentId=0&name=MOST%20VISITED")
 
-soup = BeautifulSoup(r.content)
+# soup = BeautifulSoup(r.content)
 
-need = soup.find_all("script", {'charset':'utf-8'})
-TtoWrite = str(need[1])
+# need = soup.find_all("script", {'charset':'utf-8'})
+# TtoWrite = str(need[1])
 
-TtoWrite = TtoWrite.replace("	", "")
-TtoWrite = re.search('(delivery.*)', TtoWrite).group()
-TtoWrite = TtoWrite.replace("delivery = ", "")
-TtoWrite = TtoWrite.replace(";", "")
-TtoWrite = TtoWrite.replace("\"", "")
-TtoWrite = TtoWrite.replace("manifest.f4m", "playlist.m3u8")
+# TtoWrite = TtoWrite.replace("	", "")
+# TtoWrite = re.search('(delivery.*)', TtoWrite).group()
+# TtoWrite = TtoWrite.replace("delivery = ", "")
+# TtoWrite = TtoWrite.replace(";", "")
+# TtoWrite = TtoWrite.replace("\"", "")
+# TtoWrite = TtoWrite.replace("manifest.f4m", "playlist.m3u8")
 
-videoSource = TtoWrite
+# videoSource = TtoWrite
 
-URLStream = videoSource
+# URLStream = videoSource
+URLStream = "https://www.youtube.com/watch?v=a0cQgi_y8fE"
 
 util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
