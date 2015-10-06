801f0751b2761eb9956d5bb0fadf9c0899b77b0c
RozebMomin <rozebm@gmail.com>
2015-10-06 09:49:46 -0400

    updated Aaj Tak

--- plugin.video.AajTak/default.py
+++ plugin.video.AajTak/default.py
@@ -26,6 +26,6 @@ addon = xbmcaddon.Addon('plugin.video.AajTak')
 # videoSource = TtoWrite
 
 # URLStream = videoSource
-URLStream = "plugin://plugin.video.youtube/?action=play_video&videoid=a0cQgi_y8fE"
+URLStream = "plugin://plugin.video.youtube/?action=play_video&videoid=5ZOQNkXGrBI"
 
 util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
