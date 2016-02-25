edfc4bb3ef4412b2e1acb6b3afa1a43cbf9493e9
RozebMomin <rozebm@gmail.com>
2016-02-25 14:02:13 -0500

    Updated Repo

--- plugin.video.AajTak/default.py
+++ plugin.video.AajTak/default.py
@@ -6,6 +6,6 @@ import os
 
 addon = xbmcaddon.Addon('plugin.video.AajTak')
 
-xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/?action=play_video&videoid=92BvhFGRLi4)')
+xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/play/?video_id=92BvhFGRLi4)')
 #watch?v=92BvhFGRLi4
 #util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
