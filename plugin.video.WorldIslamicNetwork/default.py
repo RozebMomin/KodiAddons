4c12f9599bee9497ae575362fe2ac1e5b0e49948
RozebMomin <rozebm@gmail.com>
2016-03-01 21:26:32 -0500

    Updated Repo

--- plugin.video.WorldIslamicNetwork/default.py
+++ plugin.video.WorldIslamicNetwork/default.py
@@ -11,7 +11,8 @@ import os
 
 addon = xbmcaddon.Addon('plugin.video.WorldIslamicNetwork')
 
-URLStream = "http://live.wmncdn.net/winislam/d042e6f2b8e7e4c40b35b857a0d4102b.sdp/playlist.m3u8"
+URLStream = "rtsp://live.wmncdn.net/winislam/d042e6f2b8e7e4c40b35b857a0d4102b.sdp"
 #http://live.wmncdn.net/winislam/d042e6f2b8e7e4c40b35b857a0d4102b.sdp/playlist.m3u8
+#rtsp://live.wmncdn.net/winislam/d042e6f2b8e7e4c40b35b857a0d4102b.sdp
 
 util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
