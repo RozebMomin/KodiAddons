72dc1f5090884ca6b441ece79cc2a9826c339cf4
RozebMomin <rozebm@gmail.com>
2016-03-08 22:06:26 -0500

    Updated Repo

--- plugin.video.MakkahTVLive/default.py
+++ plugin.video.MakkahTVLive/default.py
@@ -6,6 +6,7 @@ import os
 
 addon = xbmcaddon.Addon('plugin.video.MakkahTVLive')
 
-xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/?action=play_video&videoid=ArVmnth5jB4)')
+xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/play/?video_id=ArVmnth5jB4)')
+#plugin://plugin.video.youtube/?action=play_video&videoid=ArVmnth5jB4)')
 
 #util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
