4b2e547d59ac6e6dfd40cb95d60b359c138600a1
RozebMomin <rozebm@gmail.com>
2016-04-03 16:57:01 -0400

    Updated Repo

--- plugin.video.WorldIslamicNetwork/default.py
+++ plugin.video.WorldIslamicNetwork/default.py
@@ -8,7 +8,4 @@ addon = xbmcaddon.Addon('plugin.video.WorldIslamicNetwork')
 
 xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/play/?video_id=acWnaZnoGkU)')
 
-# URLStream = "rtsp://live.wmncdn.net/winislam/d042e6f2b8e7e4c40b35b857a0d4102b.sdp"
 #acWnaZnoGkU
-
-util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
