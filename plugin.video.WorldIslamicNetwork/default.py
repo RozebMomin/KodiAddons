6e8e53fcf0aa0e2f523f69be6f64f9c6006d8659
RozebMomin <rozebm@gmail.com>
2016-06-08 13:52:44 -0400

    Updated Repo

--- plugin.video.WorldIslamicNetwork/default.py
+++ plugin.video.WorldIslamicNetwork/default.py
@@ -4,6 +4,8 @@ from subprocess import call
 import json
 import os
 
+#https://www.youtube.com/watch?v=acWnaZnoGkU
+
 addon = xbmcaddon.Addon('plugin.video.WorldIslamicNetwork')
 
 xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/play/?video_id=acWnaZnoGkU)')
