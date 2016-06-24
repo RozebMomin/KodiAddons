4ed0970868fd8ff0af42a9d9e03b3d39a51500e7
RozebMomin <rozebm@gmail.com>
2016-06-24 12:19:09 -0400

    Updated Repo

---
+++ plugin.video.CartoonNetwork/default.py
@@ -0,0 +1,35 @@
+import xbmcaddon, util
+import subprocess
+from subprocess import call
+import json
+import os
+import requests
+from bs4 import BeautifulSoup
+import re
+
+addon = xbmcaddon.Addon('plugin.video.CartoonNetwork')
+
+# r = requests.get("http://hellotv.in/livetv/play?classid=164217&parentId=1041&name=News")
+
+# soup = BeautifulSoup(r.content)
+
+# need = soup.find_all("script", {'charset':'utf-8'})
+# TtoWrite = str(need[1])
+
+# TtoWrite = TtoWrite.replace("	", "")
+# TtoWrite = re.search('(delivery.*)', TtoWrite).group()
+# TtoWrite = TtoWrite.replace("delivery = ", "")
+# TtoWrite = TtoWrite.replace(";", "")
+# TtoWrite = TtoWrite.replace("\"", "")
+# TtoWrite = TtoWrite.replace("manifest.f4m", "playlist.m3u8")
+
+
+# videoSource = TtoWrite
+
+# URLStream = videoSource
+
+# URLStream = "rtsp://edge-ind.inapcdn.in:1935/berry/inewsup.stream_aac"
+
+URLStream = "http://d1hya96e2cm7qi.cloudfront.net/Live/_definst_/amlst:sweetbcha1novD28L240P/chunklist_b500000.m3u8"
+
+util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
