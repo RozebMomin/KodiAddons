0e763af5045757b178584b3c5d2fdaea3ebbe799
RozebMomin <rozebm@gmail.com>
2016-06-23 22:38:32 -0400

    Updated Repo

--- plugin.video.TV9Gujarat/default.py
+++ plugin.video.TV9Gujarat/default.py
@@ -28,6 +28,8 @@ addon = xbmcaddon.Addon('plugin.video.TV9Gujarat')
 
 # URLStream = videoSource
 
-URLStream = "rtsp://edge-ind.inapcdn.in:1935/berry/inewsup.stream_aac"
+# URLStream = "rtsp://edge-ind.inapcdn.in:1935/berry/inewsup.stream_aac"
+
+URLStream = "http://d1hya96e2cm7qi.cloudfront.net/Live/_definst_/amlst:sweetbcha1novD206L240P/chunklist_b700000.m3u8"
 
 util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
