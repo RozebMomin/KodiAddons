051a471d26482d3b8d1b275f8137658f1896412c
RozebMomin <rozebm@gmail.com>
2016-07-07 13:24:44 -0400

    Updated Repo

--- plugin.video.espn_3/make_repo-plugins.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR=plugin.video.espn_3
-cd ..
-rsync -av --copy-links --delete $DIR repo-plugins/ --exclude "*.pyo" --exclude "*.pyc" --exclude ".git" --exclude ".idea" --exclude "*.sh" --exclude "bugs" --exclude ".*" --exclude "TODO"
-
