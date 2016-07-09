02f2d22eeca8068a866ff856918c4f6ce9f651dc
RozebMomin <rozebm@gmail.com>
2016-07-09 13:38:09 -0400

    Updated Repo

---
+++ plugin.video.BanglaTVLive/resources/channels.php
@@ -0,0 +1,199 @@
+<!-- NOT WORKING CHANNELS -->
+<li>
+
+
+ <a target="_top" href="//www.jagobd.com/banglavision.html" rel="bookmark" title="Banglavision"><img src="http://www.jagobd.com/wp-content/uploads/2012/11/9.jpg" class="attachment-350x300 wp-post-image" alt="Banglavision" title="" width="95" height="99">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/banglavision.html" title="Banglavision">Banglavision</a></div>
+
+</li>
+
+<li>
+
+
+ <a target="_top" href="//www.jagobd.com/channeli.html" rel="bookmark" title="Channel i BD"><img src="//www.jagobd.com/image/channel_ir.jpg" class="attachment-350x300 wp-post-image" alt="Channel i BD" title="" width="350" height="182">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/channeli.html" title="Channel i BD">Channel i BD</a></div>
+
+</li>
+
+<li>
+
+
+ <a target="_top" href="//www.jagobd.com/atn-bangla.html" rel="bookmark" title="ATN Bangla"><img src="//jagobd.com/images/atnb.jpg" class="attachment-350x300 wp-post-image" alt="ATN Bangla" title="ATN Bangla" width="169" height="176">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/atn-bangla.html" title="ATN Bangla">ATN Bangla</a></div>
+
+</li>
+
+<li>
+
+
+ <a target="_top" href="//www.jagobd.com/btvworld.html" rel="bookmark" title="BTV World"><img src="//jagobd.com/image/btvworldss.jpg" class="attachment-350x300 wp-post-image" alt="BTV World" title="" width="125" height="88">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/btvworld.html" title="BTV World">BTV World</a></div>
+</li>
+
+<li>
+
+
+  <a target="_top" href="//www.jagobd.com/boishakhitv.html" rel="bookmark" title="Boishakhi TV"><img src="//jagobd.com/image/Z8iRD.jpg" class="attachment-350x300 wp-post-image" alt="Boishakhi TV" title="Boishakhi TV" width="125" height="88">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/boishakhitv.html" title="Boishakhi TV">Boishakhi TV</a></div>
+
+
+
+</li>
+
+<li>
+
+
+
+
+  <a target="_top" href="//www.jagobd.com/news24tv.html" rel="bookmark" title="NEWS24 TV"><img src="//jagobd.com/images/news24s.jpg" class="attachment-350x300 wp-post-image" alt="NEWS24 TV" title="NEWS24 TV" width="125" height="88">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/news24tv.html" title="NEWS24 TV">NEWS24 TV</a></div>
+
+
+
+</li>
+
+<li>
+
+
+ <a target="_top" href="//www.jagobd.com/mohonatv.html" rel="bookmark" title="Mohonatv"><img src="//www.jagobd.com/wp-content/uploads/2012/11/24.jpg" class="attachment-350x300 wp-post-image" alt="Mohonatv" title="" width="200" height="191">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/mohonatv.html" title="Mohonatv">Mohonatv</a></div>
+
+</li>
+
+<li>
+
+
+<a target="_top" href="//www.jagobd.com/gaanbangla.html" rel="bookmark" title="GaanBangla TV"><img src="//jagobd.com/image/gb2.jpg" class="attachment-350x300 wp-post-image" alt="GaanBangla TV" title="" width="200" height="191">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/gaanbangla.html" title="GaanBangla TV">GaanBangla TV</a></div>
+
+</li>
+
+<li>
+
+ <a target="_top" href="//www.jagobd.com/bangla21tv.html" rel="bookmark" title="Bangla21 TV"><img src="//jagobd.com/image/Bangla21d.jpg" class="attachment-350x300 wp-post-image" alt="Bangla21 TV" title="EbWW5" width="132" height="99">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/bangla21tv.html" title="Bangla21 TV">Bangla21 TV</a></div>
+</li>
+
+<li>
+
+
+ 
+<a target="_top" href="//www.jagobd.com/bijoytv.html" rel="bookmark" title="Bijoy TV"><img src="//jagobd.com/image/bijoytvs.jpg" class="attachment-350x300 wp-post-image" alt="Bijoy TV" title="" width="125" height="88">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/bijoytv.html" title="Bijoy TV">Bijoy TV</a></div>
+
+</li>
+
+<li>
+
+<a target="_top" href="//www.jagobd.com/deshebideshe-tv.html" rel="bookmark" title="Deshe-Bideshe TV"><img src="//www.jagobd.com/wp-content/uploads/2016/04/deshebd.jpg" class="attachment-350x300 wp-post-image" alt="Deshe-Bideshe TV" title="" width="125" height="88">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/deshebideshe-tv.html" title="Deshe-Bideshe TV">Deshe-Bideshe TV</a></div>
+
+
+</li>
+
+<li>
+
+<a target="_top" href="//www.jagobd.com/millenniumtv.html" rel="bookmark" title="Millennium TV"><img src="//www.jagobd.com/wp-content/uploads/2014/02/millenniumtvsm.jpg" class="attachment-350x300 wp-post-image" alt="Millennium TV" title="" width="125" height="88">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/millenniumtv.html" title="Millennium TV">Millennium TV</a></div>
+
+</li>
+
+<li>
+
+<a target="_top" href="//www.jagobd.com/tbn24.html" rel="bookmark" title="TBN24"><img src="//www.jagobd.com/images/tbn24.jpg" class="attachment-350x300 wp-post-image" alt="TBN24" title="" width="125" height="88">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/tbn24.html" title="TBN24">TBN24</a></div>
+
+
+</li>
+
+<li>
+
+
+ <a target="_top" href="//www.jagobd.com/channelieurope.html" rel="bookmark" title="Channel i Europe"><img src="//www.jagobd.com/wp-content/uploads/2012/11/10.jpg" class="attachment-350x300 wp-post-image" alt="Channel i Europe" title="" width="350" height="182">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/channelieurope.html" title="Channel i Europe">Channel i Europe</a></div>
+
+
+
+</li>
+
+<li>
+
+
+ <a target="_top" href="//www.jagobd.com/ntveurope.html" rel="bookmark" title="Ntv Europe"><img src="//www.jagobd.com/wp-content/uploads/2012/11/17.jpg" class="attachment-350x300 wp-post-image" alt="Ntv Europe" title="" width="350" height="200">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/ntveurope.html" title="Ntv Europe">Ntv Europe</a></div>
+
+
+</li>
+
+<li>
+
+
+ <a target="_top" href="//www.jagobd.com/tvoneuk.html" rel="bookmark" title="TVONE UK"><img src="//www.jagobd.com/images/tvone.png" class="attachment-350x300 wp-post-image" alt="TVONE UK" title="" width="350" height="273">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/tvoneuk.html" title="TVONE UK">TVONE UK</a></div>
+
+
+</li>
+
+<li>
+
+
+
+
+
+ <a target="_top" href="//www.jagobd.com/channels-uk.html" rel="bookmark" title="Channel S UK"><img src="//www.jagobd.com/wp-content/uploads/2012/11/18.jpg" class="attachment-350x300 wp-post-image" alt="Channel S UK" title="" width="350" height="182">
+      <span class="play"></span>    </a>
+ 
+<div class="author3">
+<a target="_top" href="//www.jagobd.com/channels-uk.html" title="Channel S UK">Channel S UK</a></div>
+
+
+
+</li>
