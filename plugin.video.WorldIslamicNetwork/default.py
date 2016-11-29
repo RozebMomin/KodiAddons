3c0fa946dc0c9fb452a33367f986516f3d224aab
RozebMomin <rozebm@gmail.com>
2016-11-29 16:28:43 -0500

    WIN Channel Link Updated

--- plugin.video.WorldIslamicNetwork/default.py
+++ plugin.video.WorldIslamicNetwork/default.py
@@ -4,10 +4,10 @@ from subprocess import call
 import json
 import os
 
-#https://www.youtube.com/watch?v=acWnaZnoGkU
+#https://www.youtube.com/watch?v=jKrMn5Ta6iI
 
 addon = xbmcaddon.Addon('plugin.video.WorldIslamicNetwork')
 
-xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/play/?video_id=acWnaZnoGkU)')
+xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/play/?video_id=jKrMn5Ta6iI)')
 
-#acWnaZnoGkU
+#jKrMn5Ta6iI
