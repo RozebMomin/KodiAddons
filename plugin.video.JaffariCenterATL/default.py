543730e0db9f6faa5cd359c9ea80246e5a6c8dbe
RozebMomin <rozebm@gmail.com>
2016-12-04 12:22:52 -0500

    Updated Repo

--- plugin.video.JaffariCenterATL/default.py
+++ plugin.video.JaffariCenterATL/default.py
@@ -3,9 +3,22 @@ import subprocess
 from subprocess import call
 import json
 import os
+from bs4 import BeautifulSoup
+import urllib
+
+
+url = "https://www.youtube.com/user/JaffariCenterATL/live"
+
+page3 = urllib.urlopen(url).read()
+soup3 = BeautifulSoup(page3)
+
+desc = soup3.findAll(attrs={"itemprop":"videoId"}) 
+url2 = desc[0]['content'].encode('utf-8')
 
 addon = xbmcaddon.Addon('plugin.video.JaffariCenterATL')
 
-xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/?action=play_video&videoid=Sje18clUrqw)')
+xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/?action=play_video&videoid='+url+')')
 
 #util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), URLStream)
+
+
