773d53da8d24a743502e17f7d9fa7313fb4df767
RozebMomin <rozebm@gmail.com>
2016-12-06 13:12:22 -0500

    Updated Ahlulbayt TV
    updated live stream link

--- plugin.video.AhlulbaytTV.en/default.py
+++ plugin.video.AhlulbaytTV.en/default.py
@@ -2,4 +2,4 @@ import xbmcaddon, util
 
 addon = xbmcaddon.Addon('plugin.video.AhlulbaytTV.en')
 
-util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), 'rtmp://109.123.126.10:1935/live/livestream3.sdp')
+util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), 'rtmp://109.123.126.10:1935/live/livestream1.sdp')
