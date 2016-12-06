2e9516a795222cc7aefab72de0d6120aef677599
RozebMomin <rozebm@gmail.com>
2016-12-06 17:26:26 -0500

    Removed unused addons
    ABP News
    Ask Maulana
    Channel Pear
    Saudi Quran
    MyFoxAtlantaLive

--- plugin.video.SaudiQuranTV.en/default.py
+++
@@ -1,5 +0,0 @@
-import xbmcaddon, util
-
-addon = xbmcaddon.Addon('plugin.video.SaudiQuranTV.en')
-
-xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/?action=play_video&videoid=Yu2lmo3TrfY)')
