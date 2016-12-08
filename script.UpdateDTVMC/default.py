5ffd47b0159aaf3eddcae4a981d955b864205ebb
RozebMomin <rozebm@gmail.com>
2016-12-07 19:16:11 -0500

    Updated Script

--- script.UpdateDTVMC/default.py
+++ script.UpdateDTVMC/default.py
@@ -2,23 +2,26 @@ import os
 import os.path
 import urllib
 import zipfile
+import xbmc, xbmcgui
 
 
-fname = "/storage/.kodi/addons/repository.shani"
+fileNameOne = "/storage/.kodi/addons/repository.shani"
 
 getFile = urllib.URLopener()
 sourcePathFileOne = "http://askmaulana.com/streams/repository.shani-2.9.zip"
 targetPathFileOne = "/tmp/repository.shani-2.9.zip"
 extractionDirectory = "/storage/.kodi/addons/"
 
-if os.path.exists(fname):
+if os.path.exists(fileNameOne):
     print "Path already exists."
+    xbmcgui.Dialog().notification('DesiTV Media Center', 'Shani Repository Already Exists!', xbmcgui.NOTIFICATION_WARNING, 4000)
 else:
-    print "Path does not exist. Retrieving ZIP file."
+	xbmcgui.Dialog().notification('DesiTV Media Center', 'Downloading Shani Repository', xbmcgui.NOTIFICATION_INFO, 2000)
     getFile.retrieve(sourcePathFileOne, targetPathFileOne)
     print "Successfully retrieved file."
     print "Unzipping ZIP file to target directory."
     zip_ref = zipfile.ZipFile(targetPathFileOne, 'r')
     zip_ref.extractall(extractionDirectory)
     zip_ref.close()
+    xbmcgui.Dialog().notification('DesiTV Media Center', 'Successfully Installed Shani Repository.', 2000)
     print "Successfully unzipped file to target directory."
