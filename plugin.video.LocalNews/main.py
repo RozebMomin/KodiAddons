0983d2fcad5691ff9c171fdd3415422bb1e8d973
RozebMomin <rozebm@gmail.com>
2019-06-22 12:31:43 -0400

    Updated Local News 3.0

--- plugin.video.LocalNews/main.py
+++ plugin.video.LocalNews/main.py
@@ -51,7 +51,7 @@ VIDEOS = {'Atlanta': [{'name': 'Watch My Fox Atlanta LIVE',
                      ],
             'Cricket World Cup': [{'name': 'Watch Cricket World Cup LIVE',
                       'thumb': 'https://images.indianexpress.com/2019/06/ind-759-3.jpg?w=759',
-                      'video': 'http://samo6.serdan.xyz/hls/skymaineventsd43/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9Ni8xNi8yMDE5IDU6MjE6NTEgUE0maGFzaF92YWx1ZT1MNlo4UW1mV0Z6Qk5qWGpYdmhSbTVRPT0mdmFsaWRtaW51dGVzPTEyMA==',
+                      'video': 'https://ul.cdn946.net:8443/hls/2vcwg84v3c6ho.m3u8?s=0gDFud0xDBKIKCG1MX1dvA&e=1561242469',
                       'genre': 'Cricket'}
                      ]}
                      
